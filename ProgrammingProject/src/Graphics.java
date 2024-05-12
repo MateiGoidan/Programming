97b2b0c9bd8e5fd6db345b82a56eb2cbf63b9487
mariana <marianavallemunoz1@gmail.com>
2024-05-12 16:53:22 +0200

    finally

--- ProgrammingProject/src/Graphics.java
+++ ProgrammingProject/src/Graphics.java
@@ -1,2 +1,33 @@
-package PACKAGE_NAME;public class Graphics {
+package com.example;
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.util.ArrayList;
+import java.util.List;
+
+public class Graphics extends JPanel implements ActionListener{
+    private static final int CANVAS_Width = 600;
+    private static final int CANVAS_HEIGHT = 400;
+    private static final int TIMER_DELAY = 30;
+
+    private List<Person> people;
+    public Graphics(){
+        this.people = new ArrayList<>();
+        this.setPreferredSize(new Dimension (CANVAS_WIDTH, CANVAS_HEIGHT));
+        this.setBackground(Color.White);
+        Timer timer= new Timer (TIMER_DELAY, this);
+        timer.start();
+
+        //Initialize a person object
+        people.add(new Person());
+    }
+
+    protected void paintComponent(Graphics g){
+        super.paintComponent(g);
+        for (Person person : people){
+            person.draw(g);
+        }
+    }
+
 }
